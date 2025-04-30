dd6dbb463f2d44f41a5468639246440dbaaf406e
brett-me <its-me-brett@protonmail.com>
2025-04-30 12:48:49 +0200

    add: ex001-hello-world

---
+++ rust-by-example/ex001-hello-world/hello.rs
@@ -0,0 +1,11 @@
+// ================================================================================================
+//  EX001: HELLO WORLD
+// ================================================================================================
+//  in this exercise, 'hello.rs' is generated manually without cargo
+//  generate the binary using <rustc 'filename'> in terminal
+//  execute binary with ./'filename'
+
+fn main() {
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
