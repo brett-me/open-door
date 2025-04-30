4b44e8308dd4e1e284f47ea7a8b994356e31717e
brett-me <its-me-brett@protonmail.com>
2025-04-30 12:52:37 +0200

    edit: --ex001-hello-world update instructions

--- rust-by-example/ex001-hello-world/hello.rs
+++ rust-by-example/ex001-hello-world/hello.rs
@@ -2,8 +2,9 @@
 //  EX001: HELLO WORLD
 // ================================================================================================
 //  in this exercise, 'hello.rs' is generated manually without cargo
-//  generate the binary using <rustc 'filename'> in terminal
-//  execute binary with ./'filename'
+//  1. create a .rs file and save code
+//  2. generate the binary using <rustc 'filename'> in terminal
+//  3. execute binary with ./'filename'
 
 fn main() {
     println!("Hello World!");
