606617ad0de71f25ddee9ab67d39b9f8334d84df
brett-me <its-me-brett@protonmail.com>
2025-05-02 10:45:53 +0200

    add: --ch03-04

---
+++ rust-book/ch03-04-comments/src/main.rs
@@ -0,0 +1,24 @@
+// ================================================================================================
+//   CH03-04 COMMENTS
+// ================================================================================================
+// idiomatic comment style starts with //
+// block comments start with /* (good for debugging)
+// generally comments are placed one line above the code they are annotating
+
+// this is a main function
+fn main() {
+    // I add a blank println here so that there's seperation between compiler text and the program
+    println!();
+    println!("Hello, world!"); // comments may also appear next to a line of code
+    // line below commented out as part of debugging process
+    // test();
+}
+
+// example using block comment
+/*
+fn test() {
+    println!("disabling this function as part of a debugging process");
+}
+*/
+
+// different types of comments will be elaborated on in other chapters
