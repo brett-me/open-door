1fee922e9c0774e6af526ed4319839e9dc5e0a27
brett-me <its-me-brett@protonmail.com>
2025-05-02 10:21:07 +0200

    add: --ch03-03-functions final part

--- rust-book/ch03-03-functions/src/main.rs
+++ rust-book/ch03-03-functions/src/main.rs
@@ -34,11 +34,29 @@ fn no_return_keyword(x: u8, y: u8) -> u8 {
 // statements are instructions that perform an action and do not return a value
 // expression evaluate to a resultant value
 
+// ================================================================================================
+// functions with return values
+// ================================================================================================
+// return type must be declared in function signature with ->
+// return value synonymous with the value of the final expression (implicit)
+// use the 'return' keyword to return early (after a condition for example)
+
+fn passkey() -> u64 {
+    3450872235
+}
+// function above is valid. It simply returns a value as a u64 data type
+
 fn main() {
+    println!();
     sans_parameters();
+    println!();
     let x: String = String::from("hooray!");
     one_parameter(&x);
     one_parameter(&x); // NB!!! this is only possible because the argument is a reference
+    println!();
     let total = no_return_keyword(2, 5);
     println!("2 + 5 = {total}");
+    let secret = passkey();
+    println!();
+    println!("The code is: {secret}");
 }
