0598dd76b830e5a58c57f4f0bad2c7b0c628e2cb
brett-me <its-me-brett@protonmail.com>
2025-05-08 14:49:53 +0200

    update solution: exercise move semanics1

--- rustlings/exercises/06_move_semantics/move_semantics1.rs
+++ rustlings/exercises/06_move_semantics/move_semantics1.rs
@@ -1,10 +1,7 @@
 // TODO: Fix the compiler error in this function.
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
     vec.push(88);
-
-    vec
+    vec.to_vec()
 }
 
 fn main() {
@@ -17,8 +14,8 @@ mod tests {
 
     #[test]
     fn move_semantics1() {
-        let vec0 = vec![22, 44, 66];
-        let vec1 = fill_vec(vec0);
+        let mut vec0 = vec![22, 44, 66];
+        let vec1 = fill_vec(&mut vec0);
         assert_eq!(vec1, vec![22, 44, 66, 88]);
     }
 }
