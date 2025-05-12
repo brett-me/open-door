8fe40ddf14799e133cdec04de166361535b8dc59
brett-me <its-me-brett@protonmail.com>
2025-05-12 13:54:07 +0200

    complete: ps0-02 playback (python) [& refactor rust]

---
+++ cs50-rust/ps0-02-playback/src/main.py
@@ -0,0 +1,6 @@
+def main():
+    user_input = input()
+    print(user_input.replace(' ', '...'))
+
+if __name__ == "__main__":
+    main()
--- cs50-rust/ps0-02-playback/src/main.rs
+++ cs50-rust/ps0-02-playback/src/main.rs
@@ -7,17 +7,5 @@ fn main() {
     stdin()
         .read_line(&mut user_input)
         .expect("Error reading line");
-
-    let mut output = user_input;
-    if output.ends_with('\n') {
-        output.pop();
-        if output.ends_with('\r') {
-            output.pop();
-        }
-    }
-
-    if output.contains(" ") {
-        output = output.replace(" ", "...");
-    }
-    println!("{}", output);
+    println!("{}", user_input.replace(' ', "..."));
 }
