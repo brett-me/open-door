6d3e9c266a22148d0aa9d58138909409ae3057e2
brett-me <its-me-brett@protonmail.com>
2025-05-13 17:09:15 +0200

    add: ch06-02-03 option<T>

---
+++ rust-book/ch06-02-03-optionT/src/main.rs
@@ -0,0 +1,18 @@
+// ================================================================================================
+// CH 06-02-03 MATCHING WITH OPTION<T>
+// ================================================================================================
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}", six);
+    println!("{:?}", none);
+}
