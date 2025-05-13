1462031892fc4a21ee680267ca4c423f813156cc
brett-me <its-me-brett@protonmail.com>
2025-05-13 11:41:35 +0200

    add: ch06-01-02 enum continued

--- rust-book/ch06-01-02-enum-cont/src/main.rs
+++ rust-book/ch06-01-02-enum-cont/src/main.rs
@@ -1,3 +1,23 @@
+// ================================================================================================
+// CH 06-01-02 ENUM CONTINUED
+// ================================================================================================
+
+// message enum whose variants each store different amounts and types of values
+#[derive(Debug)]
+enum Message {
+    Quit,                       // no data
+    Move { x: i32, y: i32 },    // named fields (like a struct)
+    Write(String),              // single String
+    ChangeColor(i32, i32, i32), // three i32 values
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", &self);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
