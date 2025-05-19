de02fb6a80a0e21e35f5d895e69474f4dc7329e9
brett-me <its-me-brett@protonmail.com>
2025-05-19 14:52:55 +0200

    refactor: ps1-2 bank

--- cs50-rust/ps1-02-bank/src/main.rs
+++ cs50-rust/ps1-02-bank/src/main.rs
@@ -1,27 +1,22 @@
 use std::io::stdin;
 
 fn main() {
-    println!("Greeting: ");
+    println!("Greeting:");
     let greeting: String = get_input();
-    let cash: String = return_cash(greeting);
+    let cash: String = return_cash(&greeting); // pass a reference into return cash (function does not alter argument)
     println!("{cash}");
 }
 
 fn get_input() -> String {
-    let mut empty_line = String::new();
-    stdin()
-        .read_line(&mut empty_line)
-        .expect("error reading line");
-    let greeting: String = empty_line.trim().to_lowercase();
-    greeting
+    let mut greeting = String::new();
+    stdin().read_line(&mut greeting).expect("String input");
+    greeting.trim().to_lowercase() // no need to declare a new variable, apply methods directly to return value
 }
 
-fn return_cash(user_input: String) -> String {
-    if user_input.starts_with("hello") {
-        return "$0".into();
-    } else if user_input.starts_with("h") {
-        return "$20".into();
-    } else {
-        return "$100".into();
-    };
+fn return_cash(user_input: &str) -> String {
+    match user_input {
+        s if s.starts_with("hello") => "$0".into(), // nb: evoke variable s, so match arm is returnable
+        s if s.starts_with("h") => "$20".into(),
+        _ => "$100".into(), // _ serves as a catchall wildcard (must be the final match expression)
+    }
 }
