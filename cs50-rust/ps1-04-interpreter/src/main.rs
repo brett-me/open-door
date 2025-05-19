2f255063c3a905d71f0b7cdb5ce40d8160c1ef65
brett-me <its-me-brett@protonmail.com>
2025-05-19 21:31:51 +0200

    refactor: ps1-04 rust solution

--- cs50-rust/ps1-04-interpreter/src/main.rs
+++ cs50-rust/ps1-04-interpreter/src/main.rs
@@ -1,33 +1,40 @@
-use std::io::stdin;
+use std::io::{self, Write};
 
 fn main() {
     println!("Expression:");
+    io::stdout().flush().unwrap();
     let expression = get_expression();
     println!("{:.1}", calculate(&expression));
 }
 
 fn get_expression() -> String {
     let mut expression = String::new();
-    stdin()
+    io::stdin()
         .read_line(&mut expression)
         .expect("int (+, -, *, /) int");
     expression.trim().to_lowercase()
 }
 
 fn calculate(expression: &str) -> f32 {
-    let elements: Vec<&str> = expression.split(' ').collect();
+    let elements: Vec<&str> = expression.split_whitespace().collect();
     if elements.len() != 3 {
         return 0.0;
-    } else {
-        let x: f32 = elements[0].parse::<f32>().expect("number");
-        let operator = elements[1];
-        let y: f32 = elements[2].parse::<f32>().expect("number");
-        match operator {
-            "+" => x + y,
-            "-" => x - y,
-            "*" => x * y,
-            "/" => x / y,
-            _ => 0.0,
-        }
+    }
+    // use match to parse element, this avoids panic!
+    let x: f32 = match elements[0].parse() {
+        Ok(n) => n,
+        Err(_) => return 0.0,
+    };
+    let y: f32 = match elements[2].parse() {
+        Ok(n) => n,
+        Err(_) => return 0.0,
+    };
+    let operator = elements[1];
+    match operator {
+        "+" => x + y,
+        "-" => x - y,
+        "*" => x * y,
+        "/" => x / y,
+        _ => 0.0,
     }
 }
