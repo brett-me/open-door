f902838a6207b2b33f58030b5366854d48c65f23
brett-me <its-me-brett@protonmail.com>
2025-05-28 17:36:24 +0200

    complete: ex11 hashmaps

--- rustlings/exercises/11_hashmaps/hashmaps2.rs
+++ rustlings/exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,14 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        if !basket.contains_key(&fruit) {
+            let amount = match fruit {
+                Fruit::Banana => 1,
+                Fruit::Pineapple => 2,
+                _ => continue,
+            };
+            basket.insert(fruit, amount);
+        }
     }
 }
 
